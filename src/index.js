f9cd8212a428d755ee0faacfc7d659c3a63d05ae
Labreche Abdellatif <tifo.zelo@gmail.com>
2020-08-17 15:56:16 +0100

    Bootstrap setup

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { ApolloProvider } from '@apollo/client';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import './index.css';
 import App from './App';
