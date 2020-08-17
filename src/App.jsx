30c25106e78abd67a590ded8135dc8d5ef8705b8
Labreche Abdellatif <tifo.zelo@gmail.com>
2020-08-17 17:26:00 +0100

    Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+
+import Navigation from './components/Navigation';
 
 class App extends Component {
   render() {
     return (
-      <BrowserRouter></BrowserRouter>
+      <BrowserRouter>
+        <Navigation />
+        <Container className="my-4">
+          <Switch>
+            
+          </Switch>
+        </Container>
+      </BrowserRouter>
     );
   }
 }
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { 
+  Navbar, 
+  Nav, 
+  NavDropdown, 
+  Form, 
+  FormControl, 
+  Button, 
+  Container, 
+  InputGroup,
+  Badge
+} from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart, faSearch } from '@fortawesome/free-solid-svg-icons';
+
+class Navigation extends Component {
+  state = {};
+  
+  render() {
+    return (
+      <Navbar bg="dark" variant="dark" expand="lg">
+        <Container>
+          <Navbar.Brand>
+            My Store
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Form inline className="mx-auto w-50">
+              <InputGroup className="w-100">
+                <FormControl type="text" placeholder="Search" className="border-light" />
+                <InputGroup.Append>
+                  <Button variant="outline-light" className="bg-white text-secondary border-left-0">
+                    <FontAwesomeIcon icon={faSearch}></FontAwesomeIcon>
+                  </Button>
+                </InputGroup.Append>
+              </InputGroup>
+            </Form>
+            <Nav>
+              <Nav.Link as="div">
+                <FontAwesomeIcon icon={faShoppingCart}></FontAwesomeIcon>
+                <Badge pill variant="primary" className="ml-2">0</Badge>
+              </Nav.Link>
+              <NavDropdown title="Firstname" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.3">Profile</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.1">Dashboard</NavDropdown.Item>
+                <NavDropdown.Item as="div">Settings</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item>Sign Out</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+            <Nav>
+              <Button variant="primary" className="mr-sm-2">Sign Up</Button>
+              <Button variant="outline-primary">Sign In</Button>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    );
+  }
+}
+
+export default Navigation;
