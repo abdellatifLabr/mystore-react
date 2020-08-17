dcd56fa450ff56f99fb694bff288399cdfa75112
Labreche Abdellatif <tifo.zelo@gmail.com>
2020-08-17 17:47:38 +0100

    Add auth tokens to graphql request

--- src/graphql/index.js
+++ src/graphql/index.js
@@ -1,7 +1,41 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+import userProvider from '../providers/user.provider';
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:8000/graphql'
+});
+
+const authLink = setContext(async (req, { headers }) => {
+  let token = localStorage.getItem('access');
+
+  if (!token) return;
+  if (req.operationName === 'RefreshToken') return;
+
+  if (userProvider.isTokenExpired(token)) {
+    let data = await userProvider.refreshToken()
+
+    if (data.success) {
+      localStorage.setItem('access', data.token);
+      token = data.token;
+    }
+
+    if (data.errors) {
+      userProvider.signOut();
+    }
+  }
+
+  return {
+    headers: {
+      ...headers,
+      authorization: `Bearer ${token}`
+    }
+  }
+});
 
 const client = new ApolloClient({
-  uri: 'http://localhost:8000/graphql',
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache()
 });
 
