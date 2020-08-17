5efbe3ee6adb76dc84f494627458c73fd33d5828
Labreche Abdellatif <tifo.zelo@gmail.com>
2020-08-17 15:52:21 +0100

    Apollo client setup

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { ApolloProvider } from '@apollo/client';
 
 import './index.css';
 import App from './App';
 import store from './store';
+import apolloClient from './graphql';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <ApolloProvider client={apolloClient}>
+        <App />
+      </ApolloProvider>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
