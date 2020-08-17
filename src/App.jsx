585fb72176cf0fdbb10f06cb2f06ba1db64b113d
Labreche Abdellatif <tifo.zelo@gmail.com>
2020-08-17 16:04:26 +0100

    React router setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        App
-      </div>
+      <BrowserRouter></BrowserRouter>
     );
   }
 }
