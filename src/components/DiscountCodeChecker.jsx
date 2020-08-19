d71202f7e01f6a37001a749a908fa34697dae142
Labreche Abdellatif <tifo.zelo@gmail.com>
2020-08-19 11:35:57 +0100

    Improved validation

--- src/components/DiscountCodeChecker.jsx
+++ src/components/DiscountCodeChecker.jsx
@@ -24,16 +24,18 @@ class DiscountCodeChecker extends Component {
 
   checkDiscountCode() {
     this.setState({
-      code: '',
       error: null
     });
 
     discountCodeProvider.getDiscountCode(this.state.code)
       .then(discountCode => {
         if (discountCode) {
-          this.props.onDiscountCodeChecked(discountCode);
-          
-          if (discountCode.expired) this.setState({ error: 'Discount code expired!' });
+          if (discountCode.expired) {
+            this.setState({ error: 'Discount code expired!' });
+          } else {
+            this.setState({ code: '' });
+            this.props.onDiscountCodeChecked(discountCode);
+          } 
         } else {
           this.setState({ error: 'This code desn\'t exist' });
         }
@@ -44,14 +46,20 @@ class DiscountCodeChecker extends Component {
     return (
       <>
       <InputGroup>
-        <FormControl type="text" name="code" onChange={this.handleCodeChange} placeholder="Discount Code" />
+        <FormControl 
+          type="text" 
+          name="code" 
+          placeholder="Discount Code"
+          value={this.state.code} 
+          onChange={this.handleCodeChange}
+        />
         <InputGroup.Append>
           <Button variant="success" onClick={this.checkDiscountCode}>
             Redeem
           </Button>
         </InputGroup.Append>
       </InputGroup>
-      {this.state.error && <span className="text-danger">{this.state.error}</span>}
+      {this.state.error && <small className="text-danger">{this.state.error}</small>}
       </>
     );
   }
