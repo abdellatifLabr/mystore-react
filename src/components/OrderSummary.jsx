8729f34cae32fc7ae90629f5d35cb93d6dec00ad
Labreche Abdellatif <tifo.zelo@gmail.com>
2020-08-25 14:16:14 +0100

    Store shipping block

--- src/components/OrderSummary.jsx
+++ src/components/OrderSummary.jsx
@@ -17,29 +17,24 @@ class OrderSummary extends Component {
               <div>
                 {orderItem.product.name} <Badge variant="primary" pill size="sm">{orderItem.quantity}</Badge>
               </div>
-              <div className="font-weight-bold">{orderItem.cost} {orderItem.product.price.currency}</div>
+              <div className="font-weight-bold">{orderItem.cost}</div>
             </ListGroup.Item>
           ))}
           {discountCodes.map(discountCode => (
             <ListGroup.Item key={discountCode.id} className="d-flex justify-content-between text-success font-weight-bold">
               <div>
                 <h6 className="m-0">{discountCode.code}</h6>
-                <small>{discountCode.store.name} products only</small>
               </div>
               <div>-{discountCode.value}%</div>
             </ListGroup.Item>
           ))}
-          {/* <ListGroup.Item className="d-flex justify-content-between text-danger">
+          <ListGroup.Item className="d-flex justify-content-between text-danger font-weight-bold">
             <div>Shipping</div>
             <div>+10 USD</div>
           </ListGroup.Item>
-          <ListGroup.Item className="d-flex justify-content-between text-danger">
-            <div>Tax</div>
-            <div>+15 USD</div>
-          </ListGroup.Item> */}
           <ListGroup.Item className="d-flex justify-content-between font-weight-bold">
             <div>Total</div>
-            <div>{this.props.order.total} {orderItems[0].product.price.currency}</div>
+            <div>{this.props.order.total}</div>
           </ListGroup.Item>
         </ListGroup>
       </Card>
