793fb4053fb88f107d4a27bc6d161b92dbe34c1e
Labreche Abdellatif <tifo.zelo@gmail.com>
2020-08-26 12:02:17 +0100

    Got rid of StoresList comp

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,32 @@
 import React, { Component } from 'react';
+import { Row, Col } from 'react-bootstrap';
 
-import StoresList from '../components/StoresList';
+import StoreCard from '../components/StoreCard';
+import storeProvider from '../providers/store.provider';
 
 class HomePage extends Component {
+  state = {
+    stores: []
+  };
+
+  componentDidMount() {
+    storeProvider.getStores()
+      .then(stores => {
+        this.setState({ stores });
+      });
+  }
+
   render() {
     return (
       <div>
-        <StoresList />
+        <h3 className="mb-3">Most Visited Stores</h3>
+        <Row>
+          {this.state.stores.map(store => (
+            <Col md={3} key={store.id}>
+              <StoreCard store={store} />
+            </Col>
+          ))}
+        </Row>
       </div>
     );
   }
